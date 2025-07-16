c3b2bcddccacecd9a3f1eaed2c4e52826e1fd92e
pe3atejlb <shepuhin@yandex.ru>
2025-07-16 22:05:27 +0300

    verible lsp wrapper fix

--- packages/verible-wrapper/src/frontend/prefsManager.ts
+++ packages/verible-wrapper/src/frontend/prefsManager.ts
@@ -1,10 +1,11 @@
 import { inject, injectable } from '@theia/core/shared/inversify';
 import { FrontendApplicationContribution } from '@theia/core/lib/browser';
 import { PreferenceService  } from '@theia/core/lib/browser/preferences/preference-service';
-import { FrontendApplicationStateService } from '@theia/core/lib/browser/frontend-application-state';
 import { EnvVariablesServer } from '@theia/core/lib/common/env-variables';
 import URI from '@theia/core/lib/common/uri';
 import * as path from 'path';
+import { ProjectManager, LLDChangeEvent } from '@gestola/project-manager/lib/frontend/project-manager/project-manager';
+import { CommandService } from '@theia/core/lib/common/command';
 
 @injectable()
 export class VeriblePrefsManager implements FrontendApplicationContribution {
@@ -12,25 +13,48 @@ export class VeriblePrefsManager implements FrontendApplicationContribution {
     @inject(PreferenceService)
     protected readonly prefsService: PreferenceService;
 
-    @inject(FrontendApplicationStateService)
-    private readonly stateService: FrontendApplicationStateService;
-
     @inject(EnvVariablesServer)
     protected readonly envServer: EnvVariablesServer;
 
-    async onStart(): Promise<void> {
-        this.stateService.reachedState('ready').then(
-            async () => {
-                let configDirUri = new URI(await this.envServer.getConfigDirUri()).path.fsPath();
-                let veriblePath = path.join(configDirUri, 'verible', 'verible-verilog-ls');
-                let filelistPath = path.join(configDirUri, 'verible.filelist');
-                this.prefsService.updateValue("verible.path", veriblePath);
-                this.prefsService.updateValue("verible.arguments", [`--file_list_path=${filelistPath}`]);
+    @inject(ProjectManager)
+    protected readonly projManager: ProjectManager;
+
+    @inject(CommandService)
+    protected readonly commandService: CommandService;
+
+    private defaultFileListPath: string;
+
+    async onStart() {
+
+        let configDirUri = new URI(await this.envServer.getConfigDirUri()).path.fsPath();
+        let veriblePath = path.join(configDirUri, 'verible', 'verible-verilog-ls');
+        let filelistPath = path.join(configDirUri, 'verible.filelist');
+        this.defaultFileListPath = filelistPath;
+
+        this.prefsService.updateValue("verible.path", veriblePath);
+        this.prefsService.updateValue("verible.arguments", [`--file_list_path=${this.defaultFileListPath}`]);
+
+        this.projManager.onDidChangeLLD((event: LLDChangeEvent) => {
+            this.setFilelistPath(event.lld.rtlModel.veribleFilelistUri.path.fsPath());
+        });
+    
+        this.projManager.onDidDesignFilesInclude(() => {
+            if(this.projManager.getCurrRTLModel()){
+                this.setFilelistPath(this.projManager.getCurrRTLModel()!.veribleFilelistUri.path.fsPath());
+                this.commandService.executeCommand("verible.restart");
             }
-        );
+        });
+    
+        this.projManager.onDidDesignFilesExclude(() => {
+            if(this.projManager.getCurrRTLModel()){
+                this.setFilelistPath(this.projManager.getCurrRTLModel()!.veribleFilelistUri.path.fsPath());
+                this.commandService.executeCommand("verible.restart");
+            }
+        });
+
     }
 
-    public setFilelistPath(filelistPath: string){
+    private setFilelistPath(filelistPath: string){
         this.prefsService.updateValue("verible.arguments", [`--file_list_path=${filelistPath}`]);
     }
 
